305890e5bf0e2a31f3939fc4bfe2124dca6ca283
JVLandayan <jvlandayaaan@gmail.com>
2025-09-15 16:49:45 +0800

    add vercel tracking

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { inject } from '@vercel/analytics';
+inject();
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
